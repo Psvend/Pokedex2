6a568d0970bf231db37fd58a4c773680d12a0e8f
JalokinBo <Nikolajbo@gmail.com>
2024-11-13 19:17:31 +0100

    Setting up PlaceHolders for buttons and fixing UI. PokemonList Needs to be Scrollable.

---
+++ app/src/main/java/com/example/pokedex2/ui/theme/MainPage.kt
@@ -0,0 +1,113 @@
+package com.example.pokedex2.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pokedex2.R
+import com.example.pokedex2.data.DatasourcePokemon
+
+@Composable
+fun MainPageBackGround(modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) } // Declare the expanded variable here
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFE55655))
+
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .background(Color(0xFFE55655))
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.top_bar_background), // Replace with your image name
+                contentDescription = "Top Bar Background",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 8.dp)
+                    .fillMaxWidth()
+            )
+            Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(120.dp)
+                    ) {
+                Button(
+                    onClick = { expanded = !expanded },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(top = 20.dp, end = 16.dp)
+                        .size(50.dp)
+                ) {
+
+                }
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 30.dp)
+        ) {
+            Spacer(modifier = Modifier.height(100.dp)) // Adjust the height value as needed
+
+            val layoutDirection = LocalLayoutDirection.current
+            Surface(
+                modifier = modifier
+                    .statusBarsPadding()
+                    .padding(
+                        start = WindowInsets.safeDrawing.asPaddingValues()
+                            .calculateStartPadding(layoutDirection),
+                        end = WindowInsets.safeDrawing.asPaddingValues()
+                            .calculateStartPadding(layoutDirection),
+                    )
+            ) {
+                AffirmationsList(
+                    affirmationLIST = DatasourcePokemon().loadAffirmations()
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainPagePreview() {
+    Scaffold (
+        bottomBar = { MenuBar() }
+    ) {
+        MainPageBackGround(modifier = Modifier.padding(it))
+    }
+}
