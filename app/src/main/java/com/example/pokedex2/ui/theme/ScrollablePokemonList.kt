b9e9fb9f0f358bd6d281c8d6cda5e35a5323c9c3
JalokinBo <Nikolajbo@gmail.com>
2024-11-14 00:49:43 +0100

    Removed redundant code and unused imports.

--- app/src/main/java/com/example/pokedex2/ui/theme/ScrollablePokemonList.kt
+++ app/src/main/java/com/example/pokedex2/ui/theme/ScrollablePokemonList.kt
@@ -1,36 +1,24 @@
 package com.example.pokedex2.ui.theme
 import com.example.pokedex2.model.affirmation
-import com.example.pokedex2.data.DatasourcePokemon
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -46,7 +34,6 @@ import com.example.pokedex2.R
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.RectangleShape
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @Composable
 fun AffirmationsList(affirmationLIST: List<affirmation>, modifier: Modifier = Modifier) {
