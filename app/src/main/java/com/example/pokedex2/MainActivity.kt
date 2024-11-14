6f10c2f30aaf1750985e828add7f25ad5eb942b7
benjamin <s235097@dtu.dk>
2024-11-14 15:17:19 +0100

    fjernet for main til at kunne gøre klar til at merge

--- app/src/main/java/com/example/pokedex2/MainActivity.kt
+++ app/src/main/java/com/example/pokedex2/MainActivity.kt
@@ -20,9 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Pokedex2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    PokemonPage(modifier = Modifier.padding(innerPadding))
-                }
+
             }
         }
     }
