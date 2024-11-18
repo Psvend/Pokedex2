5e4cfe43c5714bd923b010cb619d679a2aa2c4ee
JalokinBo <Nikolajbo@gmail.com>
2024-11-18 10:25:47 +0100

    Starten på, at homepage splittes op i en topbar og indhold

--- app/src/main/java/com/example/pokedex2/ui/theme/TopBar.kt
+++ app/src/main/java/com/example/pokedex2/ui/theme/TopBar.kt
@@ -1,24 +1,14 @@
 package com.example.pokedex2.ui.theme
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -28,7 +18,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,52 +26,56 @@ import com.example.pokedex2.data.DatasourcePokemon
 import com.example.pokedex2.ui.components.TypeFilterUI
 
 @Composable
-fun MainPageBackGround(modifier: Modifier = Modifier) {
-    //var expanded by remember { mutableStateOf(false) }
-
-
-        Column(
+fun TopBar() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+        //.background(Color(0xFFE55655).copy(alpha = 0.5f))
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.top_bar_background),
+            contentDescription = "Top Bar Background",
+            //alpha = 0.2f, hvis opacity skal ændres
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 8.dp)
+                .fillMaxWidth()
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+        ) {
+            Button(
+                onClick = { /*expanded = !expanded or navController.navigate("placeholder")
+                    */
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 80.dp)
-                ) {
-            //Edit header spacing
-            Spacer(modifier = Modifier.height(60.dp))
-
-            val layoutDirection = LocalLayoutDirection.current
-            Surface(
-                modifier = modifier
-                    .statusBarsPadding()
-                    .padding(
-                        start = WindowInsets.safeDrawing.asPaddingValues()
-                            .calculateStartPadding(layoutDirection),
-                        end = WindowInsets.safeDrawing.asPaddingValues()
-                            .calculateStartPadding(layoutDirection),
-                    )
+                    .align(Alignment.CenterEnd)
+                    .padding(top = 20.dp, end = 16.dp)
+                    .size(50.dp)
             ) {
-                AffirmationsList(
-                    affirmationLIST = DatasourcePokemon().loadAffirmations()
-                )
+
             }
         }
+    }
 }
 
-/*@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun TopBarView () {
     Pokedex2Theme {
         var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
 
         Scaffold(
-            bottomBar = {
+            topBar = @androidx.compose.runtime.Composable { TopBar() },
 
-                MenuBar(
-                    selectedItemIndex = selectedItemIndex,
-                    onItemSelected = { index -> selectedItemIndex = index }
-                )
-            }
-            topBar = {
-                TopBar()
+            bottomBar = {
+                MenuBar(selectedItemIndex = selectedItemIndex,
+                    onItemSelected = { index -> selectedItemIndex = index })
             }
         ) {
             MainPageBackGround(modifier = Modifier.padding(it))
@@ -96,14 +89,6 @@ fun GreetingPreview() {
                 else -> MainPageBackGround(modifier = Modifier.padding()) // Default to Home
             }
         }
+
     }
-}*/
-
-
-
-/*
-Box(
-modifier = Modifier
-.background(Color(0xFFE55655))
-//.background(Color.Transparent)
-) { */
+}
