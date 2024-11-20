54dc1fc4ad86c0ffeb57fb56444c11dd98892dea
JalokinBo <Nikolajbo@gmail.com>
2024-11-20 20:13:32 +0100

    Setup til HomeButton top. Function is in the wrong place at the moment due to testing.

--- app/src/main/java/com/example/pokedex2/ui/home/MainPage.kt
+++ app/src/main/java/com/example/pokedex2/ui/home/MainPage.kt
@@ -1,10 +1,12 @@
 package com.example.pokedex2.ui.home
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -14,6 +16,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.pokedex2.viewModel.AffirmationViewModel
 
@@ -28,10 +31,10 @@ fun MainPageBackGround(
     Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    //.padding(top = 80.dp)
+                    .padding(top = 80.dp, bottom = 80.dp)
                 ) {
             //Edit header spacing
-            //Spacer(modifier = Modifier.height(60.dp))
+            Spacer(modifier = Modifier.height(60.dp))
 
             val layoutDirection = LocalLayoutDirection.current
             Surface(
