45a620b6a3c835a9dfb11eac31ae47678faad9fd
Petrine <s235064@dtu.dk>
2025-01-09 11:19:53 +0100

    pokemon image api connection

--- app/src/main/java/com/example/pokedex2/ui/PokemonPage/PokemonPage.kt
+++ app/src/main/java/com/example/pokedex2/ui/PokemonPage/PokemonPage.kt
@@ -1,7 +1,11 @@
 package com.example.pokedex2.ui.PokePage
 
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,8 +25,131 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.example.pokedex2.viewModel.PokePageViewModel
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import com.example.pokedex2.model.Affirmation
 
 
+@Composable
+fun PokemonPage(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    //Add new items here to show
+    val pokemonDetail by viewModel.pokemonDetail.collectAsState()
+    val errorMessage by viewModel.errorMessage.collectAsState()
+    val pokemonImage by viewModel.pokemonImage.collectAsState()
+
+    // Fetch Pokémon details when the page is displayed
+    LaunchedEffect(pokemonIdOrName) {
+        viewModel.fetchPokemonDetail(pokemonIdOrName.lowercase()) // Ensure lowercase is passed
+    }
+
+
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFF9E6))
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        // Header Section
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(5.dp)
+        )
+
+        Spacer(modifier = Modifier.height(2.dp))
+
+        // Top Section - Name and Number
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            PokemonName()
+            PokemonNr(affirmation = pokemonAffirmation)
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Center Image and Like Button
+        Box(
+            modifier = Modifier
+                .size(300.dp)
+                .background(Color.LightGray, shape = RoundedCornerShape(12.dp))
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            PokemonImage(affirmation = pokemonAffirmation)
+            LikeButton(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .offset(y=20.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+    /*
+        // Types Section
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 1.dp),
+
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PokemonType(affirmation = pokemonAffirmation)
+        }
+
+     */
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Description Section
+        //PokemonDescription(affirmation = pokemonAffirmation)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        //Graph section
+        //PokemonStats(affirmation = pokemonAffirmation)
+
+
+    }
+}
+
+
+
+@Composable
+fun PokemonName() {
+    Text(
+        text = pokemonDetail?.name ?: "Loading...",
+        style = MaterialTheme.typography.headlineMedium,
+        color = Color.Black
+    )
+}
+
+
+
+@Composable
+fun PokemonImage () {
+    AsyncImage(
+        model = pokemonDetail?.sprites?.front_default,
+        contentDescription = "{pokemonDetail?.name} sprite",
+        modifier = Modifier.size(500.dp)
+    )
+}
+
+
+
+
+
+/*
 
 @Composable
 fun PokemonPage(
@@ -110,6 +237,8 @@ fun PokemonPage(
 
 }
 
+
+ */
 
 
 
