e5392babe9107835d38d83b3b87ffbb541945ce2
benjamin <s235097@dtu.dk>
2025-01-15 18:55:44 +0100

    got some delay on and showing pictures

--- app/src/main/java/com/example/pokedex2/ui/Quiz/Quiz.kt
+++ app/src/main/java/com/example/pokedex2/ui/Quiz/Quiz.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -120,17 +121,19 @@ fun Quiz(
 
                             },
                             colors = ButtonDefaults.buttonColors(
-                                contentColor = when {
+                                containerColor = when {
                                     selectedAnswer.value == option && option == pokemonDetail.value?.name -> Color.Green
-                                    selectedAnswer.value == option && option != pokemonDetail.value?.name -> Color.Red
-                                    else -> Color.White
-                                }
+                                    selectedAnswer.value == option -> Color.Red
+                                    else -> Color.Blue
+                                },
+                                contentColor = Color.White
                             ),
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(vertical = 4.dp)
                         ) {
-                            Text(text = option)
+                            Text(text = option
+                                )
             }
         }
     }
