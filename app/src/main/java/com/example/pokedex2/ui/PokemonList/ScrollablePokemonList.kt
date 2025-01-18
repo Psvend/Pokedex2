f9371f0960ca639f2f20a5d9bf3321052ab4289a
Louise Lund <s153976@dtu.dk>
2025-01-18 16:51:03 +0100

    Cleanup import

--- app/src/main/java/com/example/pokedex2/ui/PokemonList/ScrollablePokemonList.kt
+++ app/src/main/java/com/example/pokedex2/ui/PokemonList/ScrollablePokemonList.kt
@@ -1,5 +1,5 @@
 package com.example.pokedex2.ui.PokemonList
-import com.example.pokedex2.viewModel.AllPokemonsViewModel
+//import com.example.pokedex2.viewModel.AffirmationViewModel
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -31,7 +31,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -42,22 +41,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import coil.compose.rememberAsyncImagePainter
-import com.example.pokedex2.ui.PokePage.LikeButton
-import com.example.pokedex2.ui.SearchAndFilters.capitalizeFirstLetter
 import com.example.pokedex2.R
 import com.example.pokedex2.ui.SearchAndFilters.FilterOverlay
 import com.example.pokedex2.utils.RotatingLoader
 import com.example.pokedex2.viewModel.MainPageViewModel
+import com.example.pokedex2.viewModel.SearchViewModel
 import com.example.pokedex2.viewModel.SyncViewModel
-//import com.example.pokedex2.viewModel.AffirmationViewModel
 import kotlinx.coroutines.flow.filter
 
 @Composable
@@ -66,7 +58,8 @@ fun HomePokemonScroll(
     navController: NavHostController,
     modifier: Modifier = Modifier,
     syncViewModel: SyncViewModel = hiltViewModel(),
-    fetchAPIViewModel: MainPageViewModel = hiltViewModel()
+    fetchAPIViewModel: MainPageViewModel = hiltViewModel(),
+    searchViewModel: SearchViewModel = viewModel(),
 ) {
     /*
 }
